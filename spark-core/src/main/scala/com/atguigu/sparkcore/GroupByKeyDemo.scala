78d8461bc329f5549d4e6d20a432992f0cc02c9b
John Doe <30752740@qq.com>
2019-04-24 17:04:13 +0800

    更新

--- spark-core/src/main/scala/com/atguigu/sparkcore/GroupByKeyDemo.scala
+++ spark-core/src/main/scala/com/atguigu/sparkcore/GroupByKeyDemo.scala
@@ -1,5 +1,6 @@
 package com.atguigu.sparkcore
 
+import org.apache.spark.rdd.RDD
 import org.apache.spark.{SparkConf, SparkContext}
 
 /**
@@ -8,10 +9,18 @@ import org.apache.spark.{SparkConf, SparkContext}
   */
 object GroupByKeyDemo {
     def main(args: Array[String]): Unit = {
-        val conf = new SparkConf().setAppName("Practice").setMaster("local[2]")
+        val conf: SparkConf = new SparkConf().setAppName("Practice").setMaster("local[2]")
         val sc = new SparkContext(conf)
-        val rdd1 = sc.parallelize(Array("hello", "world", "atguigu", "hello", "are", "go"))
+        val rdd1 = sc.parallelize(
+            Array("hello", "world", "hello", "hello", "hello", "world"))
         
+        val rdd2: RDD[(String, Int)] = rdd1.map((_, 1))
+        val rdd4: RDD[(String, Int)] = rdd2.aggregateByKey(10)(_ + _ , _ + _)
+        /*val rdd3: RDD[(String, Iterable[Int])] = rdd2.groupByKey
+        val rdd4: RDD[(String, Int)] = rdd3.map {
+            case (key, it) => (key, it.size)
+        }*/
+        println(rdd4.collect.toList)
         sc.stop()
     }
 }
