5a157302671a371c27e362106f9f3c4e53b8c40c
John Doe <30752740@qq.com>
2019-04-26 16:37:17 +0800

    更新

--- spark-core/src/main/scala/com/atguigu/sparkcore/day03/transformation/SortByKey.scala
+++ spark-core/src/main/scala/com/atguigu/sparkcore/day03/transformation/SortByKey.scala
@@ -11,7 +11,7 @@ object SortByKey {
         val conf = new SparkConf().setAppName("Practice").setMaster("local[2]")
         val sc = new SparkContext(conf)
         val rdd1 =
-            sc.parallelize(Array((User("a", 11), 88), ((User("a", 10), 95)), ((User("a", 10), 91)), ((User("a", 16), 93)), ((User("a", 20), 95)), ((User("a", 3), 98))))
+            sc.parallelize(Array((new User("a", 11), 88), ((new User("a", 10), 95)), ((new User("a", 10), 91)), ((new User("a", 16), 93)), ((new User("a", 20), 95)), ((new User("a", 3), 98))))
         // key必须可以拍下
         val rdd2= rdd1.sortByKey(false)
         println(rdd2.collect.toList)
@@ -19,7 +19,7 @@ object SortByKey {
     }
 }
 
-case class User(name:String , age : Int) extends Ordered[User]{
+case class User(val name:String , val age : Int) extends Ordered[User]{
     override def compare(that: User): Int = {
         this.age - that.age
     }
