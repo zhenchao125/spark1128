aa7764647594f9cbcb14bcb20d60a67d7c5fe0c8
John Doe <30752740@qq.com>
2019-04-28 17:03:13 +0800

    更新

---
+++ spark-sql/src/main/scala/com/atguigu/ApiRDDDF.scala
@@ -0,0 +1,33 @@
+package com.atguigu
+
+import org.apache.spark.SparkContext
+import org.apache.spark.rdd.RDD
+import org.apache.spark.sql.types.{IntegerType, StringType, StructField, StructType}
+import org.apache.spark.sql.{DataFrame, Row, SparkSession}
+
+/**
+  * Author lzc
+  * Date 2019-04-28 16:34
+  */
+object ApiRDDDF {
+    def main(args: Array[String]): Unit = {
+        val spark: SparkSession = SparkSession
+            .builder()
+            .master("local[*]")
+            .appName("Test")
+            .getOrCreate()
+        val sc: SparkContext = spark.sparkContext
+        val rdd: RDD[(String, Int)] = sc.parallelize(Array(("lisi", 10), ("zs", 20), ("zhiling", 40)))
+        // 先在RDD中封装成 Row
+        val rowRDD: RDD[Row] = rdd.map {
+            case (name, age) => Row(name, age)
+        }
+        // 给每个字段定义类型和字段名字
+        //        val types = StructType(Array(StructField("name", StringType), StructField("age", IntegerType)))
+        val types = StructType(StructField("name", StringType) :: StructField("age", IntegerType) :: Nil)
+        val df: DataFrame = spark.createDataFrame(rowRDD, types)
+        df.show
+        spark.stop()
+        //   vscode
+    }
+}
--- spark-sql/src/main/scala/com/atguigu/SqlDemo.scala
+++ spark-sql/src/main/scala/com/atguigu/SqlDemo.scala
@@ -1,6 +1,6 @@
 package com.atguigu
 
-import org.apache.spark.sql.{DataFrame, SparkSession}
+import org.apache.spark.sql.{DataFrame, Dataset, SparkSession}
 
 /**
   * Author lzc
@@ -8,11 +8,30 @@ import org.apache.spark.sql.{DataFrame, SparkSession}
   */
 object SqlDemo {
     def main(args: Array[String]): Unit = {
+       
         val spark: SparkSession = SparkSession
             .builder()
             .master("local[*]")
             .appName("Test")
             .getOrCreate()
-        val frame: DataFrame = spark.read.json("")
+        // 在rdd -> ds rdd->df df->ds的时候需要隐式转换
+        import spark.implicits._
+        
+        // 2. spark.read...
+        val df: DataFrame = spark.read.json("C:\\Users\\lzc\\Desktop\\class_code\\2018_11_28\\06_scala\\spark1128\\spark-sql\\src\\main\\resources\\user.json")
+        
+//        df.createTempView("user")
+        
+//        spark.sql("select name from user").show
+        /*
+        df.rdd.collect.foreach{
+            row => println(row.getLong(0))
+        }*/
+        val ds: Dataset[User] = df.as[User]
+        ds.show
+        
+        spark.stop()
     }
 }
+
+case class User(name: String, age: Long)
