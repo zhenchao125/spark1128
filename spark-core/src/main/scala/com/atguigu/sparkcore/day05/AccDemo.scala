cb09eb3ecf0e02310fe44efda895e178c4f9e2a4
John Doe <30752740@qq.com>
2019-04-28 09:13:24 +0800

    更新

---
+++ spark-core/src/main/scala/com/atguigu/sparkcore/day05/AccDemo.scala
@@ -0,0 +1,33 @@
+package com.atguigu.sparkcore.day05
+
+import org.apache.spark.{SparkConf, SparkContext}
+
+/**
+  * Author lzc
+  * Date 2019-04-28 08:48
+  */
+object AccDemo {
+    def main(args: Array[String]): Unit = {
+        val conf = new SparkConf().setAppName("Practice").setMaster("local[2]")
+        val sc = new SparkContext(conf)
+        val rdd1 = sc.parallelize(Array(30, 50, 70, 60, 10, 20))
+//        val acc: LongAccumulator = sc.longAccumulator
+        val acc = new IntAccumulator
+        sc.register(acc)  // 使用前先注册
+        rdd1.foreach{
+            x => acc.add(x)
+        }
+    
+        println(acc.value)
+        sc.stop()
+    }
+}
+
+/*
+累加器
+    确实只有累加的功能
+    
+系统提供了一些常用的累加器, 主要针对值类型
+
+需要自定义累加器
+ */
---
+++ spark-core/src/main/scala/com/atguigu/sparkcore/day05/IntAccumulator.scala
@@ -0,0 +1,39 @@
+package com.atguigu.sparkcore.day05
+
+import org.apache.spark.util.AccumulatorV2
+
+class IntAccumulator extends AccumulatorV2[Int, Int]{
+    var sum = 0
+    
+    // 判断累加器是不是空
+    override def isZero: Boolean = sum == 0
+    
+    // 如果把累加器copy到executor
+    override def copy(): AccumulatorV2[Int, Int] = {
+        val acc = new IntAccumulator
+        acc.sum = sum
+        acc
+    }
+    
+    // 重置
+    override def reset(): Unit = {
+        sum = 0
+    }
+    
+    // 分区内的累加
+    override def add(v: Int): Unit = {
+        sum += v
+    }
+    
+    // 分区间的累加
+    override def merge(other: AccumulatorV2[Int, Int]): Unit = {
+        other match {
+            case o:IntAccumulator => this.sum += o.sum
+            case _ =>
+        }
+    }
+    
+    // 累加器的最终的值
+    override def value: Int = this.sum
+    
+}
