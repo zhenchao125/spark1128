8ea2ca069d860e20ac5bdda4f9d2dcf0128a1902
John Doe <30752740@qq.com>
2019-04-28 10:26:56 +0800

    更新

---
+++ spark-core/src/main/scala/com/atguigu/sparkcore/day05/MapAccmulator.scala
@@ -0,0 +1,43 @@
+package com.atguigu.sparkcore.day05
+
+import org.apache.spark.util.AccumulatorV2
+
+class MapAccmulator extends AccumulatorV2[Int, Map[String, Double]] {
+    var map = Map[String, Double]()
+    
+    override def isZero: Boolean = map.isEmpty
+    
+    
+    override def copy(): AccumulatorV2[Int, Map[String, Double]] = {
+        val acc = new MapAccmulator
+        acc.map ++= map
+        acc
+    }
+    
+    override def reset(): Unit = map = Map[String, Double]()
+    
+    
+    override def add(v: Int): Unit = {
+        map += "sum" -> (map.getOrElse("sum", 0d) + v)
+        map += "count" -> (map.getOrElse("count", 0d) + 1)
+    }
+    
+    override def merge(other: AccumulatorV2[Int, Map[String, Double]]): Unit = other match {
+        case o: MapAccmulator =>
+            map += "sum" -> (map.getOrElse("sum", 0d) + o.map.getOrElse("sum", 0d))
+            map += "count" -> (map.getOrElse("count", 0d) + o.map.getOrElse("count", 0d))
+        case _ =>
+    }
+    
+    
+    override def value: Map[String, Double] = {
+        map += "avg" -> map.getOrElse("sum", 0d) / map.getOrElse("count", 1d)
+        map
+    }
+}
+
+/*
+value :  (sum, count, avg)
+
+value: Map("sum" -> sum, ...)
+ */
---
+++ spark-core/src/main/scala/com/atguigu/sparkcore/day05/StringIntAcc.scala
@@ -0,0 +1,38 @@
+package com.atguigu.sparkcore.day05
+
+import org.apache.spark.util.AccumulatorV2
+
+class StringIntAcc extends AccumulatorV2[Any, (List[String], Int)]{
+    var list = List[String]()
+    var sum  = 0
+    
+    override def isZero: Boolean = list.isEmpty && sum == 0
+    
+    override def copy(): AccumulatorV2[Any, (List[String], Int)] = {
+        val acc = new StringIntAcc
+        acc.list = list
+        acc.sum = sum
+        acc
+    }
+    
+    override def reset(): Unit = {
+        list = List[String]()
+        sum = 0
+    }
+    
+    override def add(v: Any): Unit = v match {
+        case s: String => list :+= s
+        case i: Int =>  sum += i
+        case _ =>
+    }
+    
+    
+    override def merge(other: AccumulatorV2[Any, (List[String], Int)]): Unit = other match {
+        case acc: StringIntAcc =>
+            list ++= acc.list
+            sum += acc.sum
+        case _ =>
+    }
+    
+    override def value: (List[String], Int) = (list, sum)
+}
