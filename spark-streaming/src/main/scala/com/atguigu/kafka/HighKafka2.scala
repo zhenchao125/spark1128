a9f0cbf6004ca8acbf1b78129c21116352ef0483
John Doe <30752740@qq.com>
2019-04-30 09:02:45 +0800

    更新

--- spark-streaming/src/main/scala/com/atguigu/kafka/HighKafka2.scala
+++ spark-streaming/src/main/scala/com/atguigu/kafka/HighKafka2.scala
@@ -3,7 +3,6 @@ package com.atguigu.kafka
 import kafka.serializer.StringDecoder
 import org.apache.kafka.clients.consumer.ConsumerConfig
 import org.apache.spark.SparkConf
-import org.apache.spark.streaming.dstream.InputDStream
 import org.apache.spark.streaming.kafka.KafkaUtils
 import org.apache.spark.streaming.{Seconds, StreamingContext}
 
@@ -11,31 +10,36 @@ import org.apache.spark.streaming.{Seconds, StreamingContext}
   * Author lzc
   * Date 2019-04-29 16:37
   */
-object HighKafka {
-    
-    def main(args: Array[String]): Unit = {
+object HighKafka2 {
+    def createSSC(): StreamingContext = {
         // kafka 参数
         //kafka参数声明
         val brokers = "hadoop201:9092,hadoop202:9092,hadoop203:9092"
         val topic = "spark1128"
         val group = "bigdata"
-        val deserialization = "org.apache.kafka.common.serialization.StringDeserializer"
         
         val conf: SparkConf = new SparkConf().setMaster("local[*]").setAppName("WordCount")
         
         // 2. 使用SparkConf创建StreamingContext
+        
         val sctx = new StreamingContext(conf, Seconds(5))
+        sctx.checkpoint("./ck2")
         var params: Map[String, String] = Map(
             ConsumerConfig.BOOTSTRAP_SERVERS_CONFIG -> brokers,
             ConsumerConfig.GROUP_ID_CONFIG -> group
-            /*ConsumerConfig.KEY_DESERIALIZER_CLASS_CONFIG -> deserialization,
-            ConsumerConfig.VALUE_DESERIALIZER_CLASS_CONFIG -> deserialization*/
         )
         
         val kafkaDStream =
             KafkaUtils.createDirectStream[String, String, StringDecoder, StringDecoder](sctx, params, Set(topic))
         kafkaDStream.print
-        sctx.start()
-        sctx.awaitTermination()
+        sctx
+    }
+    def main(args: Array[String]): Unit = {
+        
+        
+        val ssc: StreamingContext = StreamingContext.getActiveOrCreate("./ck2", createSSC )
+    
+        ssc.start()
+        ssc.awaitTermination()
     }
 }
